73c995cbca66a4aadd1865f7b8cfdc869966e2e0
Alex Soble <asoble@gmail.com>
2016-09-28 15:35:08 -0500

    React-ify the questions HTML

--- javascripts/components/questions.js
+++ javascripts/components/questions.js
@@ -6,7 +6,90 @@
 
     render: function () {
       return dom.div({ className: 'column' },
-        dom.h2({}, 'Questions')
+        this.renderIdentitySection(),
+        this.renderResidencySection(),
+        this.renderIncomeSection(),
+        this.renderCitizenshipSection()
+      );
+    },
+
+    renderIdentitySection: function () {
+      return dom.section({},
+        dom.h3({}, 'Identity'),
+        dom.p({}, 'Check the ones you have easy access to:'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'State I.D.'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Birth Certificate'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Social Security Card'),
+        dom.br({})
+      );
+    },
+
+    renderResidencySection: function () {
+      return dom.section({},
+        dom.h3({}, 'Residency'),
+        dom.input({ type: 'radio' }),
+        dom.label({}, 'Rent'),
+        dom.br({}),
+        dom.input({ type: 'radio' }),
+        dom.label({}, 'Own Home'),
+        dom.br({}),
+        dom.input({ type: 'radio' }),
+        dom.label({}, 'Shelter'),
+        dom.br({}),
+        dom.input({ type: 'radio' }),
+        dom.label({}, 'Staying With Family or Friends')
+      );
+    },
+
+    renderIncomeSection: function () {
+      return dom.section({},
+        dom.h3({}, 'Income'),
+        dom.p({}, 'Which of the following sources of income do you receive?'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Employment'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Unemployment Benefits'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Retirement (SSI)'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Self-Employment'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Disability Benefits'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Child Support'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'Rental Income'),
+        dom.br({}),
+        dom.input( { type: 'checkbox' }),
+        dom.label({}, 'None of the Above'),
+        dom.br({})
+      );
+    },
+
+    renderCitizenshipSection: function () {
+      return dom.section({},
+        dom.h3({}, 'Citizenship'),
+        dom.p({}, 'Is everyone in your household a U.S. citizen?'),
+        dom.br({}),
+        dom.input({ type: 'radio' }),
+        dom.label({}, 'Yes'),
+        dom.br({}),
+        dom.input({ type: 'radio' }),
+        dom.label({}, 'No'),
+        dom.br({})
       );
     },
 
