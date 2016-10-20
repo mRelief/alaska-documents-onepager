71b84f970cb9ece95c22fae5e5347c5d4fcf037e
Alex Soble <asoble@gmail.com>
2016-10-20 17:12:37 -0500

    Add in Alaska rules for staying with family and friends / in a shelter

--- javascripts/components/residency_results.js
+++ javascripts/components/residency_results.js
@@ -14,11 +14,43 @@
     },
 
     render: function () {
-      if (this.props.hasStateId) return null;
+      if (this.props.hasStateId) {
+        return null;
+      } else if (this.props.stayingInShelter) {
+        return this.renderStayingInShelter();
+      } else if (this.props.stayingWithFamilyOrFriends) {
+        return this.renderStayingWithFamilyOrFriends();
+      } else {
+        return this.renderSuggestedResidencyDocuments();
+      };
+    },
 
+    renderStayingInShelter: function () {
       return dom.div({},
         dom.br({}),
-        dom.p({ className: 'result' }, 'You will need ONE of these documents to prove residency:'),
+        dom.p({ className: 'result' },
+          'You do not need to submit any documents to prove residency, ' +
+          'just state that you are staying in a shelter.'
+        )
+      );
+    },
+
+    renderStayingWithFamilyOrFriends: function () {
+      return dom.div({},
+        dom.br({}),
+        dom.p({ className: 'result' },
+          'You do not need to submit any documents to prove residency, ' +
+          'just state that you are staying with family or friends.'
+        )
+      );
+    },
+
+    renderSuggestedResidencyDocuments: function () {
+      return dom.div({},
+        dom.br({}),
+        dom.p({ className: 'result' },
+          'You will need ONE of these documents to prove residency:'
+        ),
         dom.br({}),
         dom.ul({}, this.suggestedResidencyDocumentsList())
       );
